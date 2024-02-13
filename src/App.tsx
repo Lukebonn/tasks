26d23dda2b22d327fb1a1da4e037756af800749c
BOGUS <BOGUS@EMAIL>
2024-02-13 10:51:27 -0500

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript by Luke Bonniwell
+                UD CISC275 with Hello World by Luke Bonniwell
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
