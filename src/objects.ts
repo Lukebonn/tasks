256bc490044bbeabeb535b442b381e44c399d09f
BOGUS <BOGUS@EMAIL>
2024-02-27 14:51:17 -0500

    publishquestion function

--- src/objects.ts
+++ src/objects.ts
@@ -125,6 +125,13 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
+    if (question.published) {
+        question.published = false;
+    } else {
+        question.published = true;
+    }
+    // let newPublished: boolean = question.published ? false : true;
+    // question.published = newPublished;
     return question;
 }
 
