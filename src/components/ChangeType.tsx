8502a774d3bd973878eb78c7ae5a0161a1242db2
BOGUS <BOGUS@EMAIL>
2024-03-06 01:48:22 -0500

    functions, but did not pass tests

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,23 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [type, setType] = useState<QuestionType>("short_answer_question");
+    function changeTheType(): QuestionType {
+        console.log("Current type: " + type);
+        return type === "multiple_choice_question"
+            ? "short_answer_question"
+            : "multiple_choice_question";
+    }
+    return (
+        <span>
+            <Button onClick={() => setType(changeTheType())}>
+                Change Type
+            </Button>
+            {type === "short_answer_question" ? (
+                <div data-testid="multiple-choice-text">Multiple Choice</div>
+            ) : (
+                <div data-testid="short-answer-text">Short Answer</div>
+            )}
+        </span>
+    );
 }
