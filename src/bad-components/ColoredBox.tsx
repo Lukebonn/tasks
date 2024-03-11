8083a2c8b6b9c6d14af1d7f46bce78d81a28e045
BOGUS <BOGUS@EMAIL>
2024-03-10 23:43:18 -0400

    all tests pass

--- src/bad-components/ColoredBox.tsx
+++ src/bad-components/ColoredBox.tsx
@@ -3,41 +3,48 @@ import { Button } from "react-bootstrap";
 
 export const COLORS = ["red", "blue", "green"];
 const DEFAULT_COLOR_INDEX = 0;
+// export const [colorIndex, setColorIndex] =
+//     useState<number>(DEFAULT_COLOR_INDEX);
 
-function ChangeColor(): JSX.Element {
+export function ColoredBox(): JSX.Element {
     const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
+
+    const changeColor = () => {
+        setColorIndex((1 + colorIndex) % COLORS.length);
+    };
     return (
-        <Button onClick={() => setColorIndex((1 + colorIndex) % COLORS.length)}>
-            Next Color
-        </Button>
+        <div>
+            <h3>Colored Box</h3>
+            <span>The current color is: {COLORS[colorIndex]}</span>
+            <div>
+                <ChangeColor changeColor={changeColor}></ChangeColor>
+                <ColorPreview colorIndex={colorIndex} />
+            </div>
+        </div>
     );
 }
 
-function ColorPreview(): JSX.Element {
+function ChangeColor({
+    changeColor
+}: {
+    changeColor: () => void;
+}): JSX.Element {
+    return <Button onClick={changeColor}>Next Color</Button>;
+}
+
+// eslint-disable-next-line react/prop-types
+function ColorPreview({ colorIndex }: { colorIndex: number }): JSX.Element {
     return (
         <div
             data-testid="colored-box"
             style={{
                 width: "50px",
                 height: "50px",
-                backgroundColor: COLORS[DEFAULT_COLOR_INDEX],
+                backgroundColor: COLORS[colorIndex],
                 display: "inline-block",
                 verticalAlign: "bottom",
                 marginLeft: "5px"
             }}
         ></div>
-    );
-}
-
-export function ColoredBox(): JSX.Element {
-    return (
-        <div>
-            <h3>Colored Box</h3>
-            <span>The current color is: {COLORS[DEFAULT_COLOR_INDEX]}</span>
-            <div>
-                <ChangeColor></ChangeColor>
-                <ColorPreview></ColorPreview>
-            </div>
-        </div>
     );
 }
