6ec46347637129706ecddc0d3436e8aeccfac46d
BOGUS <BOGUS@EMAIL>
2024-03-17 20:21:23 -0400

    edit mode and colorchange components

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,48 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function ChangeColor(): JSX.Element {
+    const colors = [
+        "red",
+        "blue",
+        "green",
+        "orange",
+        "purple",
+        "cyan",
+        "magenta",
+        "white",
+        "black"
+    ];
+    const [chosenColor, setChosenColor] = useState(colors[0]);
+    function updateColor(color: string) {
+        setChosenColor(color);
+    }
     return (
         <div>
-            <h3>Change Color</h3>
+            <Form>
+                {colors.map((color) => (
+                    <Form.Check
+                        inline
+                        type="radio"
+                        key={color}
+                        id={color}
+                        label={color}
+                        value={color}
+                        checked={chosenColor === color}
+                        onChange={() => updateColor(color)}
+                    />
+                ))}
+            </Form>
+            <div
+                data-testid="colored-box"
+                style={{
+                    backgroundColor: chosenColor,
+                    padding: "10px",
+                    marginTop: "10px"
+                }}
+            >
+                Currently selected color: {chosenColor}
+            </div>
         </div>
     );
 }
--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,75 @@
 import React, { useState } from "react";
+import { Col, Form, Row } from "react-bootstrap";
 
 export function EditMode(): JSX.Element {
+    const [inEditMode, setInEditMode] = useState<boolean>(false);
+    const [isStudent, setIsStudent] = useState<boolean>(true);
+    const [name, setName] = useState<string>("Your Name");
+
+    function updateEditMode(event: React.ChangeEvent<HTMLInputElement>) {
+        setInEditMode(event.target.checked);
+    }
+
+    function updateIsStudent(event: React.ChangeEvent<HTMLInputElement>) {
+        setIsStudent(event.target.checked);
+    }
+
+    function handleNameChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setName(event.target.value);
+    }
+
     return (
         <div>
-            <h3>Edit Mode</h3>
+            <Form.Group as={Row} controlId="in-edit-mode">
+                <Form.Label column sm="2">
+                    EditMode
+                </Form.Label>
+                <Col sm="10">
+                    <Form.Switch
+                        type="switch"
+                        id="editModeSwitch"
+                        name="student"
+                        label=""
+                        checked={inEditMode}
+                        onChange={updateEditMode}
+                    />
+                </Col>
+            </Form.Group>
+            {inEditMode && (
+                <Form.Group as={Row} controlId="edit-name">
+                    <Form.Label column sm="2">
+                        Name
+                    </Form.Label>
+                    <Col sm="10">
+                        <Form.Control
+                            type="text"
+                            name="student"
+                            value={name}
+                            onChange={handleNameChange}
+                        />
+                    </Col>
+                </Form.Group>
+            )}
+            <div>
+                {name} is {isStudent ? "a student" : "not a student"}.
+            </div>
+            {inEditMode && (
+                <Form.Group as={Row} controlId="is-student-check">
+                    <Form.Label column sm="2">
+                        Is Student
+                    </Form.Label>
+                    <Col sm="10">
+                        <Form.Check
+                            type="checkbox"
+                            id="isStudentCheckbox"
+                            name="student"
+                            label="Student"
+                            checked={isStudent}
+                            onChange={updateIsStudent}
+                        />
+                    </Col>
+                </Form.Group>
+            )}
         </div>
     );
 }
